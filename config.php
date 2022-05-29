93d2cdc8b13987e0431f9400e30d77c1402609a8
Smart Sender <92857429+smartsendercommunity@users.noreply.github.com>
2022-05-30 00:36:48 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
 
-// Данные интеграции с InterKassa
-$mono_token = "uvxQgsYST_AbTfPfI0UeRB9htm5tQSLEn1EPycW_uKzU";
-$ss_token = "q6lYgUcGt0SCXuqzKHb0DSkw8ZPhTMcQC4UUZxhC7W6q2lzTyZjaNX7VDTwB";
+// Данные интеграции с MonoBank
+$mono_token = "";
+$ss_token = "";
 
 // Сервисные данные
-$logUrl = "https://webhook.site/monobank";
 $dir = dirname($_SERVER["PHP_SELF"]);
 $url = ((!empty($_SERVER["HTTPS"])) ? "https" : "http") . "://" . $_SERVER["HTTP_HOST"] . $dir;
 $url = explode("?", $url);
